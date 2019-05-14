693ed7ef34c80321f97eaad78fca1bbf0733eff3
E-McNeill <ericaksaunders@gmail.com>
2019-05-14 16:58:19 -0700

    updates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {data} = require('../data/data.json');
+const data = require('./data/data.json');
 
 const app = express();
 
@@ -11,7 +11,7 @@ const mainRoutes = require('./routes');
 const aboutRoutes = require('./routes/about');
 
 app.use(mainRoutes);
-app.use('/about', aboutRoutes);
+//app.use('/about', aboutRoutes);
 
 app.listen(3000,() => {
     console.log('the application is running on localhost:3000!');
