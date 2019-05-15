e6e91cc811154a71684cf4b33fa294be2f813bd0
E-McNeill <ericaksaunders@gmail.com>
2019-05-15 08:25:10 -0700

    825

--- app.js
+++ app.js
@@ -8,10 +8,22 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 const mainRoutes = require('./routes');
-const aboutRoutes = require('./routes/about');
+//const aboutRoutes = require('./routes/about');
 
 app.use(mainRoutes);
 //app.use('/about', aboutRoutes);
+
+app.use((req, res, next) => {
+    const err = new Error('Uh oh, the page isn\'t found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    //res.status(err.status);
+    res.render('error');
+});
 
 app.listen(3000,() => {
     console.log('the application is running on localhost:3000!');
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const data = require('./data/data.json');
+
 
 router.get('/', (req, res) => {
     res.locals.projects = data.projects;
